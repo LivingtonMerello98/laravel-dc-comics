5310a147be4d5ca1aded078e5b623eb00caff891
LivingtonMerello98 <livington.merello@gmail.com>
2024-07-12 19:38:01 +0200

    [upd]-ComicController updated

---
+++ app/Http/Controllers/Admin/ComicController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Comic;
+use Illuminate\Http\Request;
+
+class ComicController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $comics = Comic::all(); //comando prendi tutti i comics dalla tab comics
+        return view('comics.index', compact('comics'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('comics.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //validazione dei dati
+        $request->validate([
+            'title' => 'required',
+            'description' => 'required',
+            'thumb' => 'required',
+            'price' => 'required',
+            'series' => 'required',
+            'sale_date' => 'required',
+            'type' => 'required'
+        ]);
+
+        $comic = new Comic();
+        $comic->title = $request->input('title');
+        $comic->description = $request->input('description');
+        $comic->thumb = $request->input('thumb');
+        $comic->price = $request->input('price');
+        $comic->series = $request->input('series');
+        $comic->sale_date = $request->input('sale_date');
+        $comic->type = $request->input('type');
+
+        $comic->save(); // Salva i dati nel database
+
+        //la rotta può essere modificata
+        return redirect()->route('comics.index')->with('success', 'Comic inserito con successo.');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id) //restituisce il modello pasta utilizzando l id
+    {
+        return view('comics.show', compact('comic'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
