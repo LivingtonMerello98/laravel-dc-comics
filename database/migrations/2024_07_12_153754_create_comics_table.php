45f373f4b83e3186d7de13ac59f2b8645d6b0e5d
LivingtonMerello98 <livington.merello@gmail.com>
2024-07-12 17:59:39 +0200

    [upd] database created

--- database/migrations/2024_07_12_153754_create_comics_table.php
+++ database/migrations/2024_07_12_153754_create_comics_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 30);
+            $table->text('description');
+            $table->string('thumb'); //url dell immagine
+            $table->decimal('price', 8, 2);
+            $table->string('series', 30);
+            $table->date('sale_date');
+            $table->string('artist', 30);
+            $table->string('writers');
+            $table->string('type', 30);
             $table->timestamps();
         });
     }
